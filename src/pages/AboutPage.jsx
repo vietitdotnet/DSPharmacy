4c9594ae542a8435e6fee5ea3e747432816417c6
tiendatmagic <tiendatmagic8@gmail.com>
2024-08-10 21:29:14 +0700

    update link to and css login

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,30 +1,37 @@
-import { Component } from 'react'
+import { Component } from "react";
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 export default class AboutPage extends Component {
   render() {
     return (
-      <div className='flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4'>
-        <form className="flex max-w-md flex-col gap-4">
-            <div>
-                <div className="mb-2 block">
-                <Label htmlFor="email1" value="Your email" />
-                </div>
-                <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+      <div className="flex flex-wrap items-center justify-center max-w-screen-xl p-4">
+        <div className="w-full h-full mx-auto">
+        <form className="flex flex-col max-w-md gap-4 mx-auto">
+          <div>
+            <div className="block mb-2">
+              <Label htmlFor="email1" value="Your email" />
             </div>
-            <div>
-                <div className="mb-2 block">
-                <Label htmlFor="password1" value="Your password" />
-                </div>
-                <TextInput id="password1" type="password" required />
+            <TextInput
+              id="email1"
+              type="email"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </div>
+          <div>
+            <div className="block mb-2">
+              <Label htmlFor="password1" value="Your password" />
             </div>
-            <div className="flex items-center gap-2">
-                <Checkbox id="remember" />
-                <Label htmlFor="remember">Remember me</Label>
-            </div>
-            <Button type="submit">Submit</Button>
-            </form>
+            <TextInput id="password1" type="password" required />
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="remember" className="cursor-pointer" />
+            <Label htmlFor="remember" className="cursor-pointer select-none">Remember me</Label>
+          </div>
+          <Button type="submit" className="bg-blue-500">Submit</Button>
+        </form>
+        </div>
       </div>
-    )
+    );
   }
 }
